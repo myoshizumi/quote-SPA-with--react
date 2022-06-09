0ca03a611a5b5ab9259dd78e38374fb0b9e6ba4b
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-06-10 07:56:16 +0900

    Add outputting data on the detail page

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,20 @@
-const NewQuotes = () => {
+import { useHistory } from 'react-router-dom'; 
+
+import QuoteForm from '../components/quotes/QuoteForm';
+
+const NewQuote = () => {
+
+  const history = useHistory();
+
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+    history.push('/quotes');
+  }
+
+
   return (
-  <h1>New Quotes Page</h1>
+    <QuoteForm onAddQuote={ addQuoteHandler }/>
 )
  };
 
-export default NewQuotes;
+export default NewQuote;
